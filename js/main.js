2d934b27d9faff2d4b026293687455ac52316713
Danuta Przydrozna <danprzy@gmail.com>
2018-06-25 14:34:20 +0200

    Add carousel with clients

--- js/main.js
+++ js/main.js
@@ -456,6 +456,7 @@ $(function(){
 		navigationSection();
 
 		goToTop();
+		clientsCarousel();
 
 
 		// Animations
@@ -490,6 +491,37 @@ $('#operation-panel a').click(function (e) {
   $(this).tab('show')
 })
 */
+
+		// carousel
+var clientsCarousel = function() {
+  var carouselList = $('#carousel ul');
+    function changeSlide() {
+        carouselList.animate({'marginLeft': -144}, 1000, moveFirstSlide);
+    }
+    setInterval(changeSlide, 3000); 
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem); // za ostatnim elementem dodaj pierwszy
+        carouselList.css({marginLeft: 0}); //zresetuje lewy margines z powrotem na wartość 0
+    }
+/*
+    //kręcenie pod przycisk w prawo
+    $('#next').on('click', function () {
+        carouselList.animate({'marginLeft': -200}, 1000, moveFirstSlide);
+    });
+
+    //kręcenie pod przycisk w lewo
+    $('#prev').on('click', function () {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        firstItem.before(lastItem);// dodawanie przed pierwszym elmentem ostatniego
+        carouselList.css({marginLeft: -200}).animate({'marginLeft': 0}, 1000);//zrób animacje od -400 do 0, trwającą 1000ms
+    });
+
+    */
+};
 
 
 
